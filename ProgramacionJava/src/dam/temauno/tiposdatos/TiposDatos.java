3ca003a14420a1c70ca942ecc6d9629c24c0128c
rafa <rafaelmendez@iesantoniogala.es>
2021-10-21 11:22:15 +0200

    Solución de la actividad 1.4 y realización de la actividad 1.5

--- ProgramacionJava/src/dam/temauno/tiposdatos/TiposDatos.java
+++ ProgramacionJava/src/dam/temauno/tiposdatos/TiposDatos.java
@@ -24,6 +24,36 @@ public class TiposDatos {
 		//tipo de datos primitivo, double (doble precisión)
 		double distanciaD = 2345.4566;
 	    System.out.println(distanciaDouble+distanciaD);
+	    
+	    //solución de la actividad 1.4
+	    //apartado 1
+	    int var1=10; //variable de tipo entero
+	    long var2=100; //variable de tipo entero largo
+	    float var3=(float)12.456; //variable de tipo decimal simple presición
+	    
+	    var1 = (int)var2;
+	    var3 = var2;	    
+	    //apartado 2	    
+	    char ch = 'c';
+	    int num = 88;
+	    //había que hacer un casting explícito
+	    ch = (char)num;
+	    
+	    //actividad 1.5
+	    //declaración de una variable de tipo byte
+	    byte b; 
+	    //inicialización de una variable entera
+        int i = 257; 
+        //inicialización de una variable decimal
+        double d = 323.142;
+        //casting explícito de int a byte
+        b = (byte) i; 
+        //salida por consola de las variables int y byte
+        System.out.println("i = " + i + " b = " + b);        
+        //casting explícito de double a byte
+        b = (byte) d; 
+        //salida por consola de las variables double y byte
+        System.out.println("d = " + d + " b= " + b);
 	}
 
 }
