d9d250469daf42441d140ca58951ceedba222d99
rafa <rafaelmendez@iesantoniogala.es>
2021-10-31 07:27:15 +0100

    Se introducen comentarios Javadoc

--- ProgramacionJava/src/dam/temados/poo/Automovil.java
+++ ProgramacionJava/src/dam/temados/poo/Automovil.java
@@ -1,11 +1,9 @@
 package dam.temados.poo;
 /**
- * Clase automóvil para trabajar con conceptos de POO
+ * Clase automóvil para trabajar con conceptos de POO, su clase base es Vehiculo
  * @author rafa
  * @version 1.0
  */
 public class Automovil extends Vehiculo{	
-	
-	
 	
 }
