9e3aade8925b4155e25ffde3531d96b3156eb21e
rafa2m <rafaelmendez@iesantoniogala.es>
2021-11-11 08:04:34 +0100

    Solución propuesta para la actividad 1.10

---
+++ ProgramacionJava/src/dam/temauno/operadores/OperadorAsignacion.java
@@ -0,0 +1,20 @@
+package dam.temauno.operadores;
+/**
+ * Solución propuesta para la actividad 1.10
+ * @author rafa
+ *
+ */
+public class OperadorAsignacion {
+	public static void main(String[] args) {
+		//inicialización de la variable entera numero
+		int numero=0;
+		//operador asignación con incremento
+		numero += 2;
+		numero *=4;
+		numero -=2;
+		numero /=3;
+		
+
+	}
+
+}
