24127c71b173b2d936b48fc45713905acdde00df
rafa2m <rafaelmendez@iesantoniogala.es>
2021-11-23 13:34:41 +0100

    Solución propuesta para la actividad 3.12

---
+++ ProgramacionJava/src/dam/tematres/bucles/SieteEnSiete.java
@@ -0,0 +1,14 @@
+package dam.tematres.bucles;
+/**
+ * Solución propuesta para la actividad 3.12
+ * @author rafa
+ * @version 1.0
+ */
+public class SieteEnSiete {
+	public static void main(String[] args) {
+		//Se recorren los números comprendidos entre 100 
+		//y 0 inclusives de 7 en 7
+		for(int i=100;i>-1;i-=7)
+			System.out.println(i);
+	}
+}
