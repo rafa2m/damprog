c7cf3e36f99d10e35bb621d3161ba5a1f3598731
rafa2m <rafaelmendez@iesantoniogala.es>
2021-12-11 19:18:34 +0100

    Solución propuesta para la actividad 3.16. Cálculo del factorial de un número dado

---
+++ ProgramacionJava/src/dam/tematres/bucles/Factorial.java
@@ -0,0 +1,31 @@
+package dam.tematres.bucles;
+
+import java.util.Scanner;
+/**
+ * Solución propuesta para la actividad 3.16
+ * Cálculo del factorial de un número dado
+ * @author rafa
+ *
+ */
+public class Factorial {
+
+	public static void main(String[] args) {
+		int numero,factorial=1;
+		Scanner in = new Scanner(System.in);
+		System.out.println("Introduzca un número entero positivo:");
+		numero = in.nextInt();
+		if(numero<0) {
+			System.out.println("El número introducido no es correcto");
+			System.exit(-1);
+		}
+		if(numero==0) 
+			System.out.println("Por convenio el factorial de 0 es " +
+					factorial);
+		else {
+			for(int i=numero;i>1;i--) {
+				factorial *=i;
+			}
+		    System.out.println("El factorial de " + numero + " es " + factorial);
+		}
+	}
+}
