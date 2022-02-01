9df48c80cb76785468afda65654539e173e17b6c
rafa2m <rafaelmendez@iesantoniogala.es>
2022-02-01 21:57:18 +0100

    Relaciones entre objetos de distintas clases

--- ProgramacionJava/src/module-info.java
+++ ProgramacionJava/src/module-info.java
@@ -2,6 +2,10 @@ module ProgramacionJava {
 	requires java.base;		
 	requires java.desktop;
 	requires java.logging;
+	requires org.apache.logging.log4j;
+	requires org.eclipse.jdt.annotation;
 	opens dam.temauno.cadenas;
 	opens dam.temauno.fechahora;
+	opens dam.tematres.logger;
+	opens dam.tematres.logapache;
 }
