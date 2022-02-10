33a9be29e4ba34596dbe5d3df96f8240a6c4ca75
rafa2m <rafaelmendez@iesantoniogala.es>
2022-02-10 13:00:14 +0100

    Ejemplo de clase interna miembro

---
+++ ProgramacionJava/src/dam/temacuatro/interna/Contenedora.java
@@ -0,0 +1,45 @@
+package dam.temacuatro.interna;
+
+public class Contenedora {
+	private int i=1;
+	private static int k;
+	public Contenedora(int i)
+	{ 
+		this.i=i;
+	}
+	public void printContenedora (InternaMiembro interna)
+	{
+		//acceso a un miembro de la clase interna
+		System.out.println("i=" + i + "claseInterna.j=" + interna.j);
+	}
+	/**
+	 * Clase interna miembro
+	 * @author rafa
+	 * @version 1.0
+	 */
+	protected class InternaMiembro
+	{
+		private int j=2;
+		public InternaMiembro(int j)
+		{
+			this.j=j;
+		}
+		public void printInterna ()
+		{
+			//acceso a una propiedad miembro de la clase contenedora
+			System.out.println("i=" + i + "j=" + j);
+		}
+	}
+	/**
+	 * Clase interna estática
+	 * @author rafa
+	 * @version 1.0
+	 */
+	protected static class EstaticaInterna{
+		public static void inicio (int k)
+		{
+			Contenedora.k = k;
+		}
+	}
+
+}
